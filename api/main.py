79c3008322ef8a06f5ac79a3b8aa5ee3211c6bbf
Dominique <dominique.waechter@thrasos.ai>
2022-12-23 13:20:22 +0100

    👔 feat: api read target name from description

--- api/main.py
+++ api/main.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+from datetime import datetime
+import json
+import os
 
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse, JSONResponse
@@ -6,8 +9,6 @@ import joblib
 import pandas as pd
 import sklearn
 
-# from kaggle.api.kaggle_api_extended import KaggleApi
-import kaggle
 import subprocess
 
 app = FastAPI()
@@ -20,46 +21,29 @@ async def root():
 
 @app.get("/score_history")
 async def history(version: str = "**", model: str = "*"):
-    # scores = pd.concat([pd.read_csv(score) for score in Path("/kaggle_data").glob(f"{version}/*_score.csv")])
-    # return HTMLResponse(scores.to_html())
-    rep = ""
+    rep = "<style>table{border-collapse: collapse;}</style>"
     for score in Path("/kaggle_data").glob(f"{version}/{model}_score.csv"):
-        rep += f"{score.parent.name} {score.stem}</br>{pd.read_csv(score).to_html()}</br></br>"
+        version_name = score.parent.name
+        model_name = score.stem.split("_")[0]
+        rep += f"Version: {version_name} Model: <a href='/predict/{version_name}/{model_name}'>{model_name}</a>{datetime.fromtimestamp(score.stat().st_mtime).strftime('%A %d %B %Y %H:%M')}</br>{pd.read_csv(score).to_html()}</br></br>"
     return HTMLResponse(rep)
 
 
 @app.get("/predict/{version}/{model}")
 async def predict(version: str, model: str):
-    test_sentence = ["git head main"]
-    pipeline = joblib.load(Path(f"/kaggle_data/{version}/{model}_model.joblib"))
-    print(pipeline)
-    # print(pipeline.predict(["git head main"]))
-    target_names = [
-        "java",
-        "c#",
-        "javascript",
-        "python",
-        "android",
-        "c++",
-        "ios",
-        "html",
-        "php",
-        ".net",
-        "jquery",
-        "css",
-        "objective-c",
-        "c",
-        "sql",
-        "iphone",
-        "asp.net",
-        "mysql",
-        "linux",
-        "node.js",
-        "git",
+    test_sentence = [
+        "git head main merge rebase",  # git
+        "java object class main",  # java
+        "python pandas numpy",  # python
+        "request database join merge",  # sql
+        # "",
     ]
+    path = Path(f"/kaggle_data/{version}").resolve()
+    pipeline = joblib.load(path / f"{model}_model.joblib")
+    print(pipeline)
+    f = open(path / "description.json", "r")
+    target_names = json.loads(f.read())["target_names"]
     preds = pipeline.predict(test_sentence)
-    # res = dict(zip(target_names, preds[0]))
-    # print(res)
     return {
         sentence: {
             tag_name: int(pred) for tag_name, pred in zip(target_names, pred_row)
@@ -71,6 +55,7 @@ async def predict(version: str, model: str):
 @app.get("/download_history/{name}")
 async def download_history(name: str):
     # @TODO controle sur name
+    # os.mkdir(f"/kaggle_data/{name}", mode=0o755)
     process = subprocess.Popen(
         f"kaggle kernels output waechter/p5-nlp-tfidf-onevsrest -p /kaggle_data/{name}".split(),
         stdout=subprocess.PIPE,
