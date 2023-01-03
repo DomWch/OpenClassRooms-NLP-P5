3c087e57227f7d9d8c73105a28ade16f5f6c3136
Dominique <dominique.waechter@thrasos.ai>
2023-01-03 15:38:10 +0100

    🚀 deploy: api synthese scores & predict

--- api/main.py
+++ api/main.py
@@ -10,7 +10,14 @@ import joblib
 import pandas as pd
 import sklearn
 
-from api.ml_class.keras_class import KerasEmbedTransformer
+# from tensorflow.keras import preprocessing as keras_preprocessing
+from tensorflow import keras
+
+# from api.ml_class import kerasembedtransformerclass
+# from api.kerasembedtransformerclass.kerasembedtransformerclass import (
+#     KerasEmbedTransformer,
+# )
+from .kerasembedtransformerclass import kerasembedtransformerclass
 
 # from api.ml_class.keras_class import KerasEmbedTransformer as KerasEmbedTransformerClass
 
@@ -25,11 +32,31 @@ async def root():
 @app.get("/score_history")
 async def history(version: str = "**", model: str = "*"):
     rep = "<style>table{border-collapse: collapse;}</style>"
+    scoresf1 = {}
     for score in Path("/kaggle_data").glob(f"{version}/{model}_score.csv"):
         version_name = score.parent.name
         model_name = score.stem.split("_")[0]
-        rep += f"Version: {version_name} Model: <a href='/predict/{version_name}/{model_name}'>{model_name}</a>{datetime.fromtimestamp(score.stat().st_mtime).strftime('%A %d %B %Y %H:%M')}</br>{pd.read_csv(score).to_html()}</br></br>"
-    return HTMLResponse(rep)
+        # TODO verif si le model exist & si joblib sklearn ou tf keras
+        if (
+            Path(f"/kaggle_data/{version_name}/{model_name}_model.joblib")
+            .resolve()
+            .exists()
+        ):
+            link = f"<a href='/predict/{version_name}/{model_name}'>{model_name}</a>"
+        else:
+            link = model_name
+        score_df = pd.read_csv(score, index_col=0)
+        # @TODO filter naive identique
+        scoresf1[f"{version_name}_{model_name}"] = score_df["f1-score"][
+            ["micro avg", "macro avg", "weighted avg", "samples avg"]
+        ]
+        rep += f"Version: {version_name} Model: {link} {datetime.fromtimestamp(score.stat().st_mtime).strftime('%A %d %B %Y %H:%M')}</br>{score_df.to_html()}</br></br>"
+    scoresf1 = pd.DataFrame(scoresf1).T.sort_values(by="micro avg", ascending=False)
+    # return HTMLResponse(rep)
+    # print(scoresf1["micro avg"].sort_values())
+    return HTMLResponse(
+        f"<div>Synthese f1-scores par modéle</div>{scoresf1.to_html()}</br>{rep}"
+    )
 
 
 @app.get("/predict/{version}/{model}")
@@ -44,24 +71,31 @@ async def predict(version: str, model: str):
     path = Path(f"/kaggle_data/{version}").resolve()
     with open(path / "description.json", "r") as f:
         description = json.loads(f.read())
-    print("ici")
-    # KerasEmbedTransformer = KerasEmbedTransformerClass().init(description["Word2Vec"])
     pipeline = joblib.load(path / f"{model}_model.joblib")
-    print("la")
     print(pipeline)
 
     if "keras_embed_transformer" in pipeline.named_steps:
-        pipeline.named_steps["keras_embed_transformer"].load(
+        load = kerasembedtransformerclass.KerasEmbedTransformer().load(
             path / "keras", description["Word2Vec"]
         )
+        print(load)
+        # pipeline.named_steps[
+        #     "keras_embed_transformer"
+        # ] = kerasembedtransformerclass.KerasEmbedTransformer().load(
+        #     path / "keras", description["Word2Vec"]
+        # )
     preds = pipeline.predict(test_sentence)
-    return {
-        sentence: {
-            tag_name: int(pred)
-            for tag_name, pred in zip(description["target_names"], pred_row)
-        }
-        for sentence, pred_row in zip(test_sentence, preds)
-    }
+    return HTMLResponse(
+        pd.DataFrame(
+            {
+                sentence: {
+                    tag_name: int(pred)
+                    for tag_name, pred in zip(description["target_names"], pred_row)
+                }
+                for sentence, pred_row in zip(test_sentence, preds)
+            }
+        ).T.to_html()
+    )
 
 
 @app.get("/download_history/{name}")
